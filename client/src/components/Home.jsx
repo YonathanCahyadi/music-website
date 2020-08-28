fdf284712abc1f32b784d946008570526828a08c
Yonathan Cahyadi <project.cahyadiyonathan@gmail.com>
2020-08-28 19:12:50 +1000

    feature-spotify

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import MusicSegment from "./MusicSegment";
-import { Menu, Spin } from "antd";
-import { HomeOutlined, LoadingOutlined } from "@ant-design/icons";
+import { Menu, Spin, Avatar } from "antd";
+import { HomeOutlined, LoadingOutlined, UserOutlined  } from "@ant-design/icons";
 import "antd/dist/antd.css";
 const axios = require('axios').default;
 
 
-const SERVER_URL = "http://localhost:3000/api/music";
+const SERVER_MUSIC_URL = "http://localhost:3000/api/music";
+const SERVER_USER_URL = "http://localhost:3000/api/user";
 
 class Home extends Component {
 
@@ -14,10 +15,15 @@ class Home extends Component {
         super(props);
         this.state = {
             currentPage: "Home",
+            userAvatar: <UserOutlined /> ,
+            userInfo: null,
+            userName: null,
+            userAvatar: null,
             data: null
         };
 
         this.handleMenuClick = this.handleMenuClick.bind(this);
+
     }
 
     // get the data from the server side
@@ -31,21 +37,39 @@ class Home extends Component {
             }
         }
 
-        axios.get(SERVER_URL, config)
+        axios.get(SERVER_MUSIC_URL, config)
         .then((res) => {
 
            this.setState({
-                userInfo: res.data.info,
                 data: res.data.data
 
-           }, () => console.log(this.state.userInfo));
+           }, () => console.log(this.state.data));
            
         });
+    }
+
+    getUserInfo(){
+        const config = { 
+            params: {
+                access_token: this.props.access_token,
+                refresh_token: this.props.refresh_token
+            }
+        }
+
+        axios.get(SERVER_USER_URL, config)
+             .then((res) => {
+                this.setState({
+                        userInfo: res.data.info,
+                        userName: res.data.info.userName,
+                        userAvatar: res.data.info.userAvatar
+                }, () => console.log(this.state.userInfo) );
+             });
     }
 
     // make an API call
     componentDidMount(){
         this.getData();
+        this.getUserInfo();
     }
 
     handleMenuClick(e){
@@ -54,12 +78,22 @@ class Home extends Component {
         });
     }
 
+    
+
     render() { 
+
+        let menu;
+        if(this.userAvatar != null){
+            menu = <Menu.Item key={this.state.userName} icon={<Avatar src={this.state.userAvatar} shape="circle" />}  >{this.state.userName}</Menu.Item>
+        }else {
+            menu = <Menu.Item key={this.state.userName} icon={<UserOutlined />} >{this.state.userName}</Menu.Item>
+        }
+
         return ( 
             <div className="home">
                 <Menu onClick={this.handleMenuClick} selectedKeys={this.state.currentPage} mode="horizontal">
                     <Menu.Item key="Home" icon={<HomeOutlined />}>Home</Menu.Item>
-                    <Menu.Item key="Profile">Profile</Menu.Item>
+                    { menu }
                 </Menu>
                 {(this.state.data != null) ? <MusicSegment data={this.state.data} /> : <Spin indicator={<LoadingOutlined style={{ fontSize: 50 }} spin />}/>}
             </div>
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var Promise = require('promise');
 const axios = require('axios').default;
 const querystring = require('querystring');
+const { response } = require('express');
+const { info } = require('console');
 
 
 
@@ -58,10 +60,46 @@ router.get("/spotify/callback", (req, res, next) => {
 
 });
 
+router.get("/user", (req, res) => {
+  const user_info_url = "https://api.spotify.com/v1/me";
+  // constructing the required headers for making the call to the Spotify API
+  const config = {
+    headers: {
+      "Authorization" : `Bearer ${req.query.access_token}`,
+      "Content-Type": "application/json"
+    }
+  };
+
+  let resJson = {};
+  let userAvatar = null;
+  let userName = null;
+
+  // get user info 
+  axios.get(user_info_url, config)
+       .then((user_res) => {
+         userName = user_res.data.display_name;
+         if(user_res.data.images.length != 0){
+           userAvatar = user_res.data.images[0].url;
+         }
+
+        resJson = {
+          info: {
+            userName: userName,
+            userAvatar: userAvatar
+          }
+        }
+
+        // send back to the client
+        res.status(200).send(resJson);
+       });
+
+});
 
 router.get("/music", (req, res, next) => {
   if(req.query.access_token != undefined){
     
+    let resJson = {};
+
     // constructing the GET Spotify music recommendation URL
     const url = "https://api.spotify.com/v1/recommendations?" + 
                 "seed_artists=" + "4NHQUGzhtTLFvgF5SZesLK";
@@ -75,82 +113,125 @@ router.get("/music", (req, res, next) => {
       }
     };
 
-    // get the lyric 
-    const lyric_api_base_url = "https://api.lyrics.ovh/v1";
-    let lyric_api_url = [];
-    let lyrics = [];
+    
 
     // get the recommendation music from spotify
     axios.get(url, config)
          .then((spotify_res) => {
-           // constructing the url for getting the lyric and artist Images
-           spotify_res.data.tracks.map((t) => {
-              lyric_api_url.push(`${lyric_api_base_url}/${t.album.artists[0].name}/${t.name}`);
-           });
+          //  // constructing the url for getting the lyric and artist Images
+          //  spotify_res.data.tracks.map((t) => {
+          //     lyric_api_url.push(`${lyric_api_base_url}/${t.album.artists[0].name}/${t.name}`);
+          //  });
            
 
-           // getting the lyric from the lyrics.ovh API
-           const requestsLyric = lyric_api_url.map((url) => {
-                axios.get(url)
-                   .then((lyric_res) => {
-                     // Check if lyric is found
-                     if(lyric_res.data.lyrics != undefined){ 
-                        lyrics.push(lyric_res.data.lyrics);
-                        //console.log(lyric_res.data.lyrics);
-                     }else{
-                       lyrics.push(null);
-                       //console.log(lyric_res.data.error);
-                     }
+          //  // getting the lyric from the lyrics.ovh API
+          //  const requestsLyric = lyric_api_url.map((url) => {
+          //         axios.get(url)
+          //          .then((lyric_res) => {
+          //            console.log(lyric_res.data)
+          //            // Check if lyric is found
+          //            if(lyric_res.data.lyrics != undefined){ 
+          //               lyrics.push(lyric_res.data.lyrics);
+          //               //console.log(lyric_res.data.lyrics);
+          //            }else{
+          //              lyrics.push(null);
+          //              //console.log(lyric_res.data.error);
+          //            }
+          //           })
+          //          .catch((e) => lyrics.push(null)); // catch the error when getting the lyric form the lyric.ovh API
+          //         });
+
+          //  // get user Info from spotify
+          //  const user_info_url = "https://api.spotify.com/v1/me";
+          //  let userName = null;
+          //  let userAvatar = null;
+          //  const requestUserInfo = axios.get(user_info_url, config)
+          //                               .then((user_res) => {
+          //                                 console.log(user_res.data)
+          //                                 userName = user_res.data.display_name;
+          //                                 userAvatar = user_res.data.images[0].url;
+          //                                 console.log(userName);
+          //                                 console.log(userAvatar);
+          //                               })
+          //                               .catch((e) => res.status(400).send({"message": `${e}`})); // catch the error when getting the the user Info from Spotify
+
+          // extract the data from the spotify API
+          let data = [];
+          spotify_res.data.tracks.map((t, i) => {
+            data[i] = {
+              songTitle: t.name,
+              songUrl: t.external_urls.spotify,
+              artistName: t.album.artists[0].name,
+              artistImages: t.album.images[0].url,
+              previewUrl: t.preview_url,
+              popularity: t.popularity
+            };
+          });
+
+          resJson = {
+            data: data
+          }
+
+          return spotify_res.data.tracks;
+
+          //  // wait until finish getting all the lyric
+          // return Promise.all(requestsLyric)
+          //         .then((val) => { // making the data to be sended to the client
+          //           console.log(val);
+          //           let response = {};
+          //           let info = {
+          //             userName: userName,
+          //             userAvatar: userAvatar
+          //           }
+          //           let data = [];
+          //           spotify_res.data.tracks.map((t, i) => {
+          //             data[i] = {
+          //               songTitle: t.name,
+          //               songUrl: t.external_urls.spotify,
+          //               artistName: t.album.artists[0].name,
+          //               artistImages: t.album.images[0].url,
+          //               previewUrl: t.preview_url,
+          //               popularity: t.popularity,
+          //               lyric: lyrics[i]
+          //             };
+          //           });
+
+          //           response = {
+          //             info: info,
+          //             data: data
+          //           }
+
+          //           return response;
+          //       }).then((response) => res.status(200).send(response));
+
+         }).then((spotify_track) => { // get the lyric from lyric.ovh API
+
+            // get the lyric 
+            const lyric_api_base_url = "https://api.lyrics.ovh/v1";
+            let lyric_api_url = [];
+            let lyrics = [];
+
+            // constructing the url for getting the lyric and artist Images
+          //  spotify_track.map((t) => {
+          //     lyric_api_url.push(`${lyric_api_base_url}/${t.album.artists[0].name}/${t.name}`);
+          //  });
+
+           
+            Promise.allSettled(
+              spotify_track.map((t) => axios.get(`${lyric_api_base_url}/${t.album.artists[0].name}/${t.name}`)))
+                  .then((response) => {
+                    console.log(response.status)
+                    response.map(r => lyrics.push(r.data.lyrics))
+                  }).then(() => {
+                    lyrics.map((r, i) => {
+                      resJson.data[i]["lyric"] = r;
                     })
-                   .catch((e) => lyrics.push(null)); // catch the error when getting the lyric form the lyric.ovh API
-                  });
-
-           // get user Info from spotify
-           const user_info_url = "https://api.spotify.com/v1/me";
-           let userName = null;
-           let userAvatar = null;
-           const requestUserInfo = axios.get(user_info_url, config)
-                                        .then((user_res) => {
-                                          console.log(user_res.data)
-                                          userName = user_res.data.display_name;
-                                          userAvatar = user_res.data.images[0].url;
-                                          console.log(userName);
-                                          console.log(userAvatar);
-                                        })
-                                        .catch((e) => res.status(400).send({"message": `${e}`})); // catch the error when getting the the user Info from Spotify
-
-           // wait until finish getting all the lyric
-          return Promise.all(requestUserInfo)
-                  .then((val) => { // making the data to be sended to the client
-                    
-                    let response = {};
-                    let info = {
-                      userName: userName,
-                      userAvatar: userAvatar
-                    }
-                    let data = [];
-                    spotify_res.data.tracks.map((t, i) => {
-                      data[i] = {
-                        songTitle: t.name,
-                        songUrl: t.external_urls.spotify,
-                        artistName: t.album.artists[0].name,
-                        artistImages: t.album.images[0].url,
-                        previewUrl: t.preview_url,
-                        popularity: t.popularity,
-                        lyric: lyrics[i]
-                      };
-                    });
-
-                    response = {
-                      info: info,
-                      data: data
-                    }
-
-                    res.status(200).send(response);
+                    res.status(200).send(resJson) 
                   })
-                  .catch((e) => console.log(e));
-
-         }).catch((e) => { // catch the error when getting the data from Spotify API
+                  
+         })
+         
+         .catch((e) => { // catch the error when getting the data from Spotify API
            res.status(400).send({ "message": `${e}` })
          });
 
